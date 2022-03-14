832e12ff308fe0d215dd556e88285966f627294e
zhejie.li <zhejie.li@zjrongxiang.com>
2022-03-14 18:02:16 +0800

    增加开启关闭域名解析功能

--- aliyunapi/domain/descDnsRecord.py
+++ aliyunapi/domain/descDnsRecord.py
@@ -2,16 +2,21 @@ from aliyunsdkcore.client import AcsClient
 from aliyunsdkcore.acs_exception.exceptions import ClientException
 from aliyunsdkcore.acs_exception.exceptions import ServerException
 from aliyunsdkalidns.request.v20150109.DescribeDomainRecordsRequest import DescribeDomainRecordsRequest
+from aliyunsdkalidns.request.v20150109.SetDomainRecordStatusRequest import SetDomainRecordStatusRequest
 import sys
 import json
 from aliyunapi.tool.Utils import *
 
-argcheck(sys.argv, 2)
+argcheck(sys.argv, 3)
 """
-命令行运行需要2个参数，参数顺序为： ak配置文件选项名(domainakinfo,cdnakinfo), 全域名：主机记录.主域名
+命令行运行需要3个参数，参数顺序为： ak配置文件选项名(domainakinfo,cdnakinfo), 全域名：主机记录.主域名
+第3个为可选参数，是否开启解析：Enable 或 Disable
 """
 account = sys.argv[1]
 fulldomain = sys.argv[2]
+status = ''
+if len(sys.argv) == 4:
+    status = sys.argv[3]
 index = fulldomain.find('.')
 record = fulldomain[0:index]
 domain = fulldomain[index+1:]
@@ -34,10 +39,12 @@ res_json = json.loads(resstr)
 res_recordlist = AttrDict(AttrDict(res_json).DomainRecords).Record
 
 NOTFIND_FLAG = 1
+RecordId = ''
 for i in range(len(res_recordlist)):
     if res_recordlist[i]["RR"] == record:
         NOTFIND_FLAG = 0
         print("记录ID为： "+res_recordlist[i]["RecordId"])
+        RecordId = res_recordlist[i]["RecordId"]
         print("记录状态为： "+res_recordlist[i]["Status"])
         print("记录主机名为： "+res_recordlist[i]["RR"])
         print("记录类型为： "+res_recordlist[i]["Type"])
@@ -45,4 +52,14 @@ for i in range(len(res_recordlist)):
         break
 if NOTFIND_FLAG:
     print("未找到该域名解析记录")
+
+if status != '' and RecordId != '':
+    request = SetDomainRecordStatusRequest()
+    request.set_accept_format('json')
+
+    request.set_RecordId(RecordId)
+    request.set_Status(status)
+
+    response = client.do_action_with_exception(request)
+    print(str(response, encoding='utf-8'))
 
