31af8786023ac5aeca439e76d0cd75fc028081f5
zhejie.li <zhejie.li@zjrongxiang.com>
2022-03-16 11:27:50 +0800

    增加使用record_id查询dns记录函数

--- aliyunapi/tool/DomainClass.py
+++ aliyunapi/tool/DomainClass.py
@@ -1,23 +1,32 @@
-from aliyunapi.tool.Utils import domain_convert
-from aliyunapi.tool.Utils import convert_domain
-from aliyunapi.tool.Utils import AttrDict
-from aliyunsdkalidns.request.v20150109.DescribeDomainRecordsRequest import DescribeDomainRecordsRequest
-from aliyunsdkalidns.request.v20150109.AddDomainRecordRequest import AddDomainRecordRequest
-from aliyunsdkalidns.request.v20150109.SetDomainRecordStatusRequest import SetDomainRecordStatusRequest
 import json
+
+from aliyunsdkalidns.request.v20150109.AddDomainRecordRequest import AddDomainRecordRequest
+from aliyunsdkalidns.request.v20150109.DeleteDomainRecordRequest import DeleteDomainRecordRequest
+from aliyunsdkalidns.request.v20150109.DescribeDomainRecordInfoRequest import DescribeDomainRecordInfoRequest
+from aliyunsdkalidns.request.v20150109.DescribeDomainRecordsRequest import DescribeDomainRecordsRequest
+from aliyunsdkalidns.request.v20150109.SetDomainRecordStatusRequest import SetDomainRecordStatusRequest
+
+from aliyunapi.tool.Utils import AttrDict
+from aliyunapi.tool.Utils import convert_domain
+from aliyunapi.tool.Utils import domain_convert
 
 
 class DnsRecord:
     def __init__(self, client, response_format='json'):
+        """
+
+        :param client: class type, AcsClient class. 阿里云sdk的AcsClient类，加载ak，地域信息
+        :param response_format: str type, response format. 请求返回的结果格式，默认json格式
+        """
         self.client = client
         self.response_format = response_format
 
-    def get_dns_info(self, fulldomain='', pagenumber=1, pagesize=200):
+    def get_dnsinfo_by_domain(self, fulldomain: str = '', pagenumber: int = 1, pagesize: int = 200):
         """
 
-        :param fulldomain:
-        :param pagenumber:
-        :param pagesize:
+        :param fulldomain: str type, include hostname,netname. 完整域名，包含主机名和网络名，如www.baidu.com
+        :param pagenumber: int type. 请求第几页
+        :param pagesize: int type. 请求每页包含多少条记录
         :return:
         """
         hostname, domainname = domain_convert(fulldomain=fulldomain)
@@ -45,17 +54,32 @@ class DnsRecord:
             print("未找到该域名解析记录")
             return False
 
-    # rr：主机名,str
-    # recordtype：可选A，CNAME,TXT等参数,str
-    # domain：主域,str
-    # value：记录值,str
-    def add_dns_record(self, rr, record_type, domain, value):
+    def get_dnsinfo_by_id(self, record_id: str):
         """
 
-        :param rr:
-        :param record_type:
-        :param domain:
-        :param value:
+        :param record_id: DNS's record_id, func get_dns_info can return dns record info
+        :return:
+        """
+        request = DescribeDomainRecordInfoRequest()
+        request.set_accept_format(self.response_format)
+        request.set_RecordId(str(record_id))
+        response = self.client.do_action_with_exception(request)
+        res_str = str(response, encoding='utf-8')
+        res_json = json.loads(res_str)
+
+        print("记录ID为： " + AttrDict(res_json).RecordId)
+        print("记录状态为： " + AttrDict(res_json).Status)
+        print("记录主机名为： " + AttrDict(res_json).RR)
+        print("记录类型为： " + AttrDict(res_json).Type)
+        print("记录值为： " + AttrDict(res_json).Value, end='\n\n')
+
+    def add_dns_record(self, rr: str, record_type: str, domain: str, value: str):
+        """
+
+        :param rr: str type, DNS record's hostname. DNS记录的主机记录
+        :param record_type: str type, DNS record's record type. options: A,CNAME,TXT... DNS记录的记录类型，可选A，CANME...
+        :param domain: str type, DNS record's netname. DNS记录的域名名称
+        :param value: str type, DNS record's record value.
         :return:
         """
         request = AddDomainRecordRequest()
@@ -70,17 +94,28 @@ class DnsRecord:
             print("DNS记录已增加，主机名{0}，记录类型{1}，记录值{2}".format(rr, record_type, value))
             fulldomain = convert_domain(rr, domain)
             print("完整域名为：" + fulldomain)
+            res_str = str(response, encoding='utf-8')
+            res_json = json.loads(res_str)
+            record_id = AttrDict(res_json).RecordId
+            print("该条记录的RecordID为："+record_id)
 
-    def del_dns_record(self, record_id):
-        pass
-
-    # record_id,dns记录id,str
-    # status,可选Disable和Enable,str
-    def change_record_status(self, record_id, status):
+    def del_dns_record(self, record_id: str):
         """
 
-        :param record_id:
-        :param status:
+        :param record_id: DNS's record_id, func get_dns_info can return dns record info
+        :return:
+        """
+        request = DeleteDomainRecordRequest()
+        request.set_accept_format(self.response_format)
+        request.set_RecordId(str(record_id))
+        response = self.client.do_action_with_exception(request)
+        print(str(response, encoding='utf-8'))
+
+    def change_record_status(self, record_id: str, status: str):
+        """
+
+        :param record_id: str type. DNS's record_id, func get_dns_info can return dns record info
+        :param status: str type, Disable or Enable
         :return:
         """
         request = SetDomainRecordStatusRequest()
@@ -89,7 +124,3 @@ class DnsRecord:
         request.set_Status(status)
         response = self.client.do_action_with_exception(request)
         print(str(response, encoding='utf-8'))
-
-
-
-
