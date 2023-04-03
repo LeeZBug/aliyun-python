988ab834530db796b18f3da60347f5ba984988ce
zhejie.li <zhejie.li@zjrongxiang.com>
2023-04-03 14:55:01 +0800

    修改CDN加速域名的修改状态脚本，增加捕捉错误

--- aliyunapi/cdn/modifyCDNdomain.py
+++ aliyunapi/cdn/modifyCDNdomain.py
@@ -19,41 +19,48 @@ region, akid, aksrt = akconfig(account)
 client = AcsClient(akid, aksrt, region)
 
 if status == "stop":
-    print("即将停止加速域名:{0}，你有10秒钟可以思考是否输入正确".format(acceleratedomain))
-    time.sleep(10)
-    stop_request = StopCdnDomainRequest()
-    stop_request.set_accept_format('json')
-    stop_request.set_DomainName(acceleratedomain)
-    stop_response = client.do_action_with_exception(stop_request)
-    if "RequestId" in str(stop_response, encoding='utf-8'):
-        print("网站:{0} CDN停用成功".format(acceleratedomain))
-    else:
+    try:
+        print("即将停止加速域名:{0}，你有10秒钟可以思考是否输入正确".format(acceleratedomain))
+        time.sleep(10)
+        stop_request = StopCdnDomainRequest()
+        stop_request.set_accept_format('json')
+        stop_request.set_DomainName(acceleratedomain)
+        stop_response = client.do_action_with_exception(stop_request)
+        if "RequestId" in str(stop_response, encoding='utf-8'):
+            print("网站:{0} CDN停用成功".format(acceleratedomain))
+
+    except ServerException as e:
+        print(e.message)
         print("加速域名已停用或停用失败！请登录阿里云控制台进行操作！")
         exit(1)
+
 elif status == "start":
-    print("即将启用加速域名:{0}".format(acceleratedomain))
-    start_request = StartCdnDomainRequest()
-    start_request.set_accept_format('json')
-    start_request.set_DomainName(acceleratedomain)
-    start_response = client.do_action_with_exception(start_request)
-    if "RequestId" in str(start_response, encoding='utf-8'):
-        print("网站:{0} CDN启用成功".format(acceleratedomain))
-    else:
+    try:
+        print("即将启用加速域名:{0}".format(acceleratedomain))
+        start_request = StartCdnDomainRequest()
+        start_request.set_accept_format('json')
+        start_request.set_DomainName(acceleratedomain)
+        start_response = client.do_action_with_exception(start_request)
+        if "RequestId" in str(start_response, encoding='utf-8'):
+            print("网站:{0} CDN启用成功".format(acceleratedomain))
+    except ServerException as e:
+        print(e.message)
         print("加速域名已启用或不存在，启用失败！请登录阿里云控制台进行操作！")
         exit(1)
+
 elif status == "delete":
     # 没有停止的话使用API接口依然可以删除，控制台无法直接删除正启用的加速域名
-    print("即将删除加速域名:{0}，你有10秒钟可以思考是否输入正确".format(acceleratedomain))
-    time.sleep(10)
-    delete_request = DeleteCdnDomainRequest()
-    delete_request.set_accept_format('json')
-    delete_request.set_DomainName(acceleratedomain)
-    delete_response = client.do_action_with_exception(delete_request)
-    # print(str(delete_response, encoding='utf-8'))
-    if "RequestId" in str(delete_response, encoding='utf-8'):
-        print("网站:{0} CDN加速域名删除成功".format(acceleratedomain))
-    else:
+    try:
+        print("即将删除加速域名:{0}，你有10秒钟可以思考是否输入正确".format(acceleratedomain))
+        time.sleep(10)
+        delete_request = DeleteCdnDomainRequest()
+        delete_request.set_accept_format('json')
+        delete_request.set_DomainName(acceleratedomain)
+        delete_response = client.do_action_with_exception(delete_request)
+        # print(str(delete_response, encoding='utf-8'))
+        if "RequestId" in str(delete_response, encoding='utf-8'):
+            print("网站:{0} CDN加速域名删除成功".format(acceleratedomain))
+    except ServerException as e:
+        print(e.message)
         print("加速域名已删除，启用失败！请登录阿里云控制台进行操作！")
         exit(1)
-
-
